b7b9015daf247dcf409a0fe59674b2834996fc7a
Grzegorz Gutowski <grzegorz.gutowski@tcs.uj.edu.pl>
2019-05-23 12:31:38 +0200

    Perform client, worker, and stage2 tests in examples

--- examples/make.py
+++ examples/make.py
@@ -9,6 +9,15 @@ os.chdir(os.path.dirname(__file__))
 
 for example in [ os.path.dirname(task) for task in glob.glob('*/kolejka_task.json') ] :
     print('Example: '+example)
-    result = example+'-result'
+    result = 'results/'+example+'-stage2'
     if not os.path.isdir(result):
+        os.makedirs(result, exist_ok=True)
+        subprocess.run(['kolejka-worker', 'stage2', example, result])
+    result = 'results/'+example+'-worker'
+    if not os.path.isdir(result):
+        os.makedirs(result, exist_ok=True)
+        subprocess.run(['kolejka-worker', 'execute', example, result])
+    result = 'results/'+example+'-client'
+    if not os.path.isdir(result):
+        os.makedirs(result, exist_ok=True)
         subprocess.run(['kolejka-client', 'execute', example, result])
