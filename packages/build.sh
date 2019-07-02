99f894243be02506c481f784214ec56ab3a40709
Grzegorz Gutowski <grzegorz.gutowski@tcs.uj.edu.pl>
2019-07-03 00:37:28 +0200

    Collect buildinfo for debian package submission

--- packages/build.sh
+++ packages/build.sh
@@ -21,6 +21,7 @@ find Kolejka*/deb_dist -type f -name "*.dsc" -exec cp -a {} dist ";"
 find Kolejka*/deb_dist -type f -name "*.orig.tar.gz" -exec cp -a {} dist ";"
 find Kolejka*/deb_dist -type f -name "*.debian.tar.xz" -exec cp -a {} dist ";"
 find Kolejka*/deb_dist -type f -name "*_source.changes" -exec cp -a {} dist ";"
+find Kolejka*/deb_dist -type f -name "*_source.buildinfo" -exec cp -a {} dist ";"
 find dist -type f -name "*_source.changes" -exec debsign --no-conf "-mkolejka.matinf.uj.edu.pl <kolejka@matinf.uj.edu.pl>" --re-sign -S {} ";"
 
 popd >/dev/null 2>&1
