53a8bfd58476340546c0b57b688c915cfa7b3a40
Grzegorz Gutowski <grzegorz.gutowski@uj.edu.pl>
2023-10-12 20:59:57 +0200

    Better search for wheels in build script

--- packages/build.sh
+++ packages/build.sh
@@ -24,6 +24,9 @@ find Kolejka*/deb_dist -type f -name "*_source.changes" -exec cp -a {} dist ";"
 find Kolejka*/deb_dist -type f -name "*_source.buildinfo" -exec cp -a {} dist ";"
 find dist -type f -name "*_source.changes" -exec debsign --no-conf "-mkolejka.matinf.uj.edu.pl <kolejka@matinf.uj.edu.pl>" --re-sign -S {} ";"
 
-./wheel_executable.py --wheel dist/KolejkaClient-*.whl --wheel dist/KolejkaCommon-*.whl --main kolejka.client.main  dist/kolejka-client
+kolejka_common_wheel="$(find dist -name "KolejkaCommon-*.whl" |sort |tail -n 1)"
+kolejka_client_wheel="$(find dist -name "KolejkaClient-*.whl" |sort |tail -n 1)"
+
+./wheel_executable.py --wheel "${kolejka_client_wheel}" --wheel "${kolejka_common_wheel}" --main kolejka.client.main  dist/kolejka-client
 
 popd >/dev/null 2>&1
